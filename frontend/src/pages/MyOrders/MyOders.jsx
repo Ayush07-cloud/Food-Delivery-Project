3e39c2849c39a2beb80f6bd48857c6ac31effac3
Ayush <kayushkumar2710@gmail.com>
2025-09-08 01:49:36 +0530

    Creating user order page

--- frontend/src/pages/MyOrders/MyOders.jsx
+++ frontend/src/pages/MyOrders/MyOders.jsx
@@ -32,8 +32,15 @@ const MyOders = () => {
                 <div key={index} className="my-orders-order">
                     <img src={assets.parcel_icon} alt="" />
                     <p>{order.items.map((item,index)=>{
-
+                        if (index === order.items.length-1) {
+                            return item.name+" x "+item.quantity
+                        }
+                        else{
+                            return item.name+" x "+item.quantity+", "
+                        }
                     })}</p>
+                    <p>${order.amount}.00</p>
+                    <p>Items: {order.items.length}</p>
                 </div>
             )
         })}
